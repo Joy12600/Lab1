484bdaefb4444b41348b1b5b4dbcefae534bdfeb
Joseph <josephdv@uninorte.edu.co>
2024-03-11 14:21:13 -0500

    Unido

--- Lab1.py
+++ Lab1.py
@@ -2,12 +2,6 @@ import os
 
 class Nodo:
     def __init__(self, valor):
-        """
-        Crea un nuevo nodo con el valor dado.
-
-        Args:
-            valor: El valor del nodo.
-        """
         self.valor = valor
         self.izquierda = None
         self.derecha = None
@@ -15,52 +9,24 @@ class Nodo:
 
 class ArbolAVL:
     def __init__(self):
-        """
-        Crea un nuevo árbol AVL vacío.
-        """
         self.raiz = None
 
-    def imprimir_arbol(self):
-        """
-        Imprime todo el árbol AVL en orden ascendente.
-        """
-        self._imprimir_arbol(self.raiz)
+    def imprimir_arbol(self, nodo=None, nivel=0):
+        if not nodo:
+            nodo = self.raiz
+        print(' ' * nivel + str(nodo.valor))
+        if nodo.izquierda:
+            self.imprimir_arbol(nodo.izquierda, nivel + 1)
+        if nodo.derecha:
+            self.imprimir_arbol(nodo.derecha, nivel + 1)
 
-    def _imprimir_arbol(self, nodo):
-        """
-        Imprime todo el árbol AVL en orden ascendente de forma recursiva.
-
-        Args:
-            nodo: El nodo actual en el que se está imprimiendo.
-        """
-        if nodo:
-            self._imprimir_arbol(nodo.izquierda)
-            print(nodo.valor)
-            self._imprimir_arbol(nodo.derecha)
-        
     def insertar(self, valor):
-        """
-        Inserta un nuevo valor en el árbol AVL.
-
-        Args:
-            valor: El valor a insertar.
-        """
         if not self.raiz:
             self.raiz = Nodo(valor)
         else:
             self.raiz = self._insertar(self.raiz, valor)
 
     def _insertar(self, nodo, valor):
-        """
-        Inserta un nuevo valor en el árbol AVL de forma recursiva.
-
-        Args:
-            nodo: El nodo actual en el que se está insertando.
-            valor: El valor a insertar.
-
-        Returns:
-            El nodo actualizado después de la inserción.
-        """
         if not nodo:
             return Nodo(valor)
         elif valor < nodo.valor:
@@ -87,28 +53,12 @@ class ArbolAVL:
         return nodo
 
     def eliminar(self, valor):
-        """
-        Elimina un valor del árbol AVL.
-
-        Args:
-            valor: El valor a eliminar.
-        """
         if not self.raiz:
             return
         else:
             self.raiz = self._eliminar(self.raiz, valor)
 
     def _eliminar(self, nodo, valor):
-        """
-        Elimina un valor del árbol AVL de forma recursiva.
-
-        Args:
-            nodo: El nodo actual en el que se está eliminando.
-            valor: El valor a eliminar.
-
-        Returns:
-            El nodo actualizado después de la eliminación.
-        """
         if not nodo:
             return nodo
         elif valor < nodo.valor:
@@ -143,141 +93,113 @@ class ArbolAVL:
         return nodo
 
     def buscar(self, valor):
-        """
-        Busca un valor en el árbol AVL.
-
-        Args:
-            valor: El valor a buscar.
-
-        Returns:
-            El nodo que contiene el valor buscado, o None si no se encuentra.
-        """
         return self._buscar(self.raiz, valor)
 
     def _buscar(self, nodo, valor):
-        """
-        Busca un valor en el árbol AVL de forma recursiva.
-
-        Args:
-            nodo: El nodo actual en el que se está buscando.
-            valor: El valor a buscar.
-
-        Returns:
-            El nodo que contiene el valor buscado, o None si no se encuentra.
-        """
         if not nodo or nodo.valor == valor:
             return nodo
         elif valor < nodo.valor:
             return self._buscar(nodo.izquierda, valor)
         else:
             return self._buscar(nodo.derecha, valor)
-        
-    def mostrar_nivel_nodo(nodo, raiz, nivel = 0):
-        """
-        Se verifica primero que el nodo sea igual a la raiz, si es asi retornara el nivel inicial.
-        Si no, la funcion se llamara a si misma recursivamente aumentando el nivel en 1 cada vez 
-        que se verifique que el nodo no sea el nodo raiz.
-        """
-        if nodo == raiz:
-            return nivel
-        else:
-            return nivel(raiz, raiz, nivel + 1)
-        
-    def encontrar_padre(node, current_node, parent_dict):
-        """
-        Esta funcion tiene como parametros el nodo, el nodo actual y padres
-        que sirve como una lista que mapea cada nodo.
-        Entrando a una condicion que verifica si el nodo esta vacio o si el mismo es igual al nodo actual,
-        sino este entrara a un ciclo para en el que verificara si el hijo esta en el nodo, luego si el hijo
-        es parte de la lista padres.
-        Verificara que la variable padre no este vacia y retornara 
-        """
-        if node is None or node == current_node:
+
+    def mostrar_nivel_nodo(self, nodo):
+        return self._mostrar_nivel_nodo(self.raiz, nodo, 1)
+
+    def _mostrar_nivel_nodo(self, nodo_actual, nodo_buscado, nivel):
+        if nodo_actual is None:
             return None
+        if nodo_actual == nodo_buscado:
+            return nivel
+        if nodo_buscado.valor < nodo_actual.valor:
+            return self._mostrar_nivel_nodo(nodo_actual.izquierda, nodo_buscado, nivel + 1)
         else:
-            for child in node:
-                if child in parent_dict:
-                    parent = parent_dict[child]
-                    if parent is not None:
-                        return parent(parent, current_node, parent_dict)
-                else:
-                    return parent(node[child], current_node, parent_dict)
+            return self._mostrar_nivel_nodo(nodo_actual.derecha, nodo_buscado, nivel + 1)
+
+    def altura(self, nodo):
+        if not nodo:
+            return 0
+        return 1 + max(self.altura(nodo.izquierda), self.altura(nodo.derecha))
+
+    def balanceado(self, nodo):
+        if not nodo:
+            return True
+        altura_izq = self.altura(nodo.izquierda)
+        altura_der = self.altura(nodo.derecha)
+        return abs(altura_izq - altura_der) <= 1 and self.balanceado(nodo.izquierda) and self.balanceado(nodo.derecha)
+
+    def imprimir_nivel(self, raiz):
+        h = self.altura(raiz)
+        for i in range(1, h + 1):
+            self.imprimir_nivel_dado(raiz, i)
+
+    def imprimir_nivel_dado(self, raiz, nivel):
+        if not raiz:
+            return
+        if nivel == 1:
+            print(raiz.valor, end=" ")
+        elif nivel > 1:
+            self.imprimir_nivel_dado(raiz.izquierda, nivel - 1)
+            self.imprimir_nivel_dado(raiz.derecha, nivel - 1)
+
+    def encontrar_nodo(self, raiz, metrica):
+        if not raiz:
+            return []
+
+        nodos = []
+        if metrica(raiz):
+            nodos.append(raiz.valor)
+
+        nodos += self.encontrar_nodo(raiz.izquierda, metrica)
+        nodos += self.encontrar_nodo(raiz.derecha, metrica)
+
+        return nodos
+
+    def encontrar_padre(self, nodo_buscado):
+        parent_dict = {}
+        return self._encontrar_padre(self.raiz, None, nodo_buscado, parent_dict)
+
+    def _encontrar_padre(self, nodo_actual, padre_actual, nodo_buscado, parent_dict):
+        if nodo_actual is None:
+            return None
+
+        parent_dict[nodo_actual] = padre_actual
+
+        if nodo_actual == nodo_buscado:
+            return padre_actual
+
+        izquierda = self._encontrar_padre(nodo_actual.izquierda, nodo_actual, nodo_buscado, parent_dict)
+        derecha = self._encontrar_padre(nodo_actual.derecha, nodo_actual, nodo_buscado, parent_dict)
+
+        return izquierda if izquierda is not None else derecha
 
     def buscar_por_categoria_y_tamano(self, categoria, tamano_min, tamano_max):
-        """
-        Busca nodos en el árbol AVL que cumplan con una categoría y un rango de tamaño.
-
-        Args:
-            categoria: La categoría a buscar.
-            tamano_min: El tamaño mínimo permitido.
-            tamano_max: El tamaño máximo permitido.
-
-        Returns:
-            Una lista de nodos que cumplen con los criterios de búsqueda.
-        """
         resultado = []
         self._buscar_por_categoria_y_tamano(self.raiz, categoria, tamano_min, tamano_max, resultado)
         return resultado
 
     def _buscar_por_categoria_y_tamano(self, nodo, categoria, tamano_min, tamano_max, resultado):
-        """
-        Busca nodos en el árbol AVL que cumplan con una categoría y un rango de tamaño de forma recursiva.
-
-        Args:
-            nodo: El nodo actual en el que se está buscando.
-            categoria: La categoría a buscar.
-            tamano_min: El tamaño mínimo permitido.
-            tamano_max: El tamaño máximo permitido.
-            resultado: La lista en la que se almacenarán los nodos encontrados.
-        """
         if nodo:
-            if nodo.valor.categoria == categoria and tamano_min <= nodo.valor.tamano <= tamano_max:
+            if categoria <= nodo.valor.categoria <= categoria and tamano_min <= nodo.valor.tamano <= tamano_max:
                 resultado.append(nodo)
-                
+
             if nodo.valor.categoria >= categoria:
                 self._buscar_por_categoria_y_tamano(nodo.izquierda, categoria, tamano_min, tamano_max, resultado)
-                
+
             if nodo.valor.categoria <= categoria:
                 self._buscar_por_categoria_y_tamano(nodo.derecha, categoria, tamano_min, tamano_max, resultado)
 
     def _obtener_altura(self, nodo):
-        """
-        Obtiene la altura de un nodo.
-
-        Args:
-            nodo: El nodo del que se desea obtener la altura.
-
-        Returns:
-            La altura del nodo.
-        """
         if not nodo:
             return 0
         return nodo.altura
 
     def _obtener_balance(self, nodo):
-        """
-        Obtiene el factor de balance de un nodo.
-
-        Args:
-            nodo: El nodo del que se desea obtener el factor de balance.
-
-        Returns:
-            El factor de balance del nodo.
-        """
         if not nodo:
             return 0
         return self._obtener_altura(nodo.izquierda) - self._obtener_altura(nodo.derecha)
 
     def _rotar_izquierda(self, z):
-        """
-        Realiza una rotación a la izquierda en el árbol AVL.
-
-        Args:
-            z: El nodo en el que se realizará la rotación.
-
-        Returns:
-            El nodo resultante después de la rotación.
-        """
         y = z.derecha
         T2 = y.izquierda
 
@@ -290,15 +212,6 @@ class ArbolAVL:
         return y
 
     def _rotar_derecha(self, z):
-        """
-        Realiza una rotación a la derecha en el árbol AVL.
-
-        Args:
-            z: El nodo en el que se realizará la rotación.
-
-        Returns:
-            El nodo resultante después de la rotación.
-        """
         y = z.izquierda
         T3 = y.derecha
 
@@ -311,38 +224,18 @@ class ArbolAVL:
         return y
 
     def _obtener_nodo_minimo(self, nodo):
-        """
-        Obtiene el nodo con el valor mínimo en un subárbol.
-
-        Args:
-            nodo: El nodo raíz del subárbol.
-
-        Returns:
-            El nodo con el valor mínimo.
-        """
         actual = nodo
         while actual.izquierda:
             actual = actual.izquierda
         return actual
 
-    def imprimir_arbol(self, nodo=None, nivel=0):
-        """
-        Imprime el árbol AVL.
-
-        Args:
-            nodo: El nodo desde el cual comenzar a imprimir. Si es None, comenzará desde la raíz.
-            nivel: El nivel actual del árbol. Se utiliza para la indentación.
-        """
-        if not nodo:
-            nodo = self.raiz
-        print(' ' * nivel + str(nodo.valor))
-        if nodo.izquierda:
-            self.imprimir_arbol(nodo.izquierda, nivel + 1)
-        if nodo.derecha:
-            self.imprimir_arbol(nodo.derecha, nivel + 1)
-
+# Ruta de la carpeta de datos
 carpeta_datos = 'data'
+
+# Crear un árbol AVL
 arbol = ArbolAVL()
+
+# Leer y agregar valores de archivos a la estructura del árbol AVL
 for nombre_archivo in os.listdir(carpeta_datos):
     ruta_archivo = os.path.join(carpeta_datos, nombre_archivo)
     if os.path.isfile(ruta_archivo):
@@ -350,4 +243,6 @@ for nombre_archivo in os.listdir(carpeta_datos):
             # Leer el contenido del archivo e insertar el valor en el árbol AVL
             valor = archivo.read()
             arbol.insertar(valor)
+
+# Imprimir el árbol AVL
 arbol.imprimir_arbol()
