6b546e21557ffaa8a699e8b1b893457f4fad8de3
andresg13 <rodriguezan@uninorte.edu.co>
2024-03-19 11:39:24 -0500

    Update

--- Test.py
+++ Test.py
@@ -45,9 +45,58 @@ class AVLTree:
 
         if balance < -1 and key < root.right.key:
             root.right = self.right_rotate(root.right)
+            return self.left_rotate(root)    
+        return root
+    
+    def delete(self, root, key):
+        if not root:
+            return root
+
+        if key < root.key:
+            root.left = self.delete(root.left, key)
+        elif key > root.key:
+            root.right = self.delete(root.right, key)
+        else:
+            if root.left is None:
+                temp = root.right
+                root = None
+                return temp
+            elif root.right is None:
+                temp = root.left
+                root = None
+                return temp
+
+            temp = self.get_min_value_node(root.right)
+            root.key = temp.key
+            root.right = self.delete(root.right, temp.key)
+
+        if root is None:
+            return root
+
+        root.height = 1 + max(self.get_height(root.left), self.get_height(root.right))
+        balance = self.get_balance(root)
+
+        if balance > 1 and self.get_balance(root.left) >= 0:
+            return self.right_rotate(root)
+
+        if balance < -1 and self.get_balance(root.right) <= 0:
+            return self.left_rotate(root)
+
+        if balance > 1 and self.get_balance(root.left) < 0:
+            root.left = self.left_rotate(root.left)
+            return self.right_rotate(root)
+
+        if balance < -1 and self.get_balance(root.right) > 0:
+            root.right = self.right_rotate(root.right)
             return self.left_rotate(root)
 
         return root
+
+    def get_min_value_node(self, root):
+        current = root
+        while current.left is not None:
+            current = current.left
+        return current
     def get_height(self, root):
         if not root:
             return 0
@@ -168,8 +217,31 @@ class ventana_agregar(QMainWindow):
     def regresar_principal(self):  # Función para regresar a la ventana principal
         self.close()  # Cerrar la ventana de agregar
         self.ventana_principal.show()  # Mostrar la ventana principal
-class ventana_eliminar():
-    pass
+class ventana_eliminar(QMainWindow):
+     def __init__(self, avl_tree, Principal):
+        super().__init__()
+        loadUi("EliminarArbol.ui", self)
+        self.avl_tree = avl_tree
+        self.actualizar_combobox()
+        self.pushButton.clicked.connect(self.eliminar_nodo)
+        self.pushButton_2.clicked.connect(self.regresar_principal)
+     def actualizar_combobox(self):
+        if self.avl_tree.root:
+            self.comboBox.clear()
+            self.actualizar_combobox_recursivo(self.avl_tree.root)
+     def actualizar_combobox_recursivo(self, node):
+        if node is None:
+            return
+        self.actualizar_combobox_recursivo(node.left)
+        self.comboBox.addItem(str(node.key))
+        self.actualizar_combobox_recursivo(node.right)
+     def eliminar_nodo(self):
+        nodo_a_eliminar = int(self.comboBox.currentText())
+        self.avl_tree.root = self.avl_tree.delete(self.avl_tree.root, nodo_a_eliminar)
+        self.avl_tree.plot_tree()
+     def regresar_principal(self):  # Función para regresar a la ventana principal
+        self.close()  # Cerrar la ventana de agregar
+        self.ventana_principal.show()  # Mostrar la ventana principal
 class ventana_buscar():
     pass
 class tam_peso():
@@ -182,10 +254,15 @@ class Principal(QMainWindow):
         loadUi("ArbolAVL.ui", self)
         self.avl_tree = AVLTree()
         self.pushButton.clicked.connect(self.abrir_agregar)
+        self.pushButton_2.clicked.connect(self.abrir_eliminar)
     def abrir_agregar(self):
         self.ventana_agregar = ventana_agregar(self.avl_tree, self)  # Crear una instancia de ventana_agregar
         self.hide()
         self.ventana_agregar.show() 
+    def abrir_eliminar(self):
+        self.ventana_eliminar=ventana_eliminar(self.avl_tree,self)
+        self.hide()
+        self.ventana_eliminar.show()
 if __name__ == "__main__": 
     app = QApplication(sys.argv)
     my_app = Principal()
